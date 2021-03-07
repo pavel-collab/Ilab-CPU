5d5f2cf4f23d8dd67449569e972b393b9cccc9e8
pavel-collab <rubikpf2002@mail.ru>
2021-03-07 11:53:30 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o assembler assembler.cpp
---
+++ assembler.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "commands.h"
+
+//! всегда менять формат текстового файла на LF вместо CRLF!!!!!!!!!!!!!!!!!!!!!
+
+int main() {
+
+    FILE* command_list = fopen("commands.txt", "rb");
+    assert(command_list != NULL);
+    FILE* assembler = fopen("commands_codes.txt", "wb");
+    assert(assembler != NULL);
+
+    // переменная, куда записывается прочитанная строка из файла
+    char string[20];
+    // указатель, в который будет помещен адрес массива, в который прочитана строка 
+    // (фактически адрес переменной string) или NULL
+    char* estr = fgets(string, 20, command_list);
+
+    char* keyword = NULL;
+    int i = 0;
+
+    while (estr != NULL) {
+        keyword = strtok(string, " \n");
+
+        while (keyword != NULL) {
+        printf("%s\n", keyword);
+            if (!strcmp(keyword, "push")) {
+                fprintf(assembler, "%d\n", PUSH);
+            }
+            else if (!strcmp(keyword, "pop")) {
+                fprintf(assembler, "%d\n", POP);
+            }
+            else if (!strcmp(keyword, "add")) {
+                fprintf(assembler, "%d\n", ADD);
+            }
+            else if (!strcmp(keyword, "mul")) {
+                fprintf(assembler, "%d\n", MUL);
+            }
+            else if (!strcmp(keyword, "div")) {
+                fprintf(assembler, "%d\n", DIV);
+            }
+            else if (!strcmp(keyword, "out")) {
+                fprintf(assembler, "%d\n", OUT);
+            }
+            else if (!strcmp(keyword, "end")) {
+                fprintf(assembler, "%d\n", END);
+            }
+            else {
+                fprintf(assembler, "%d\n", atoi(keyword));
+            }
+
+            keyword = strtok(NULL, " \n");
+        }
+
+        estr = fgets(string, 20, command_list);
+    }
+
+    fclose(command_list);
+    fclose(assembler);
+
+    return 0;
+}
+
+// TODO: написать ассерты
+// TODO: написать тело CPU, подумать, как связать это с файлом stck
